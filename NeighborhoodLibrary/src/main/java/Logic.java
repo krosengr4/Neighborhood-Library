4f4c54c7940fa3667ce1d9c552044ef7300d1d1b
krosengr4 <rosenkev4@gmail.com>
2025-06-30 22:37:51 -0500

    Added method to process home screen and empty methods to show all books, show checked out books, return a book, and checkout a book

--- NeighborhoodLibrary/src/main/java/Logic.java
+++ NeighborhoodLibrary/src/main/java/Logic.java
@@ -1,2 +1,37 @@
 public class Logic {
+
+	public static void processHomeScreen() {
+		boolean ifContinue = true;
+
+		while(ifContinue) {
+			int userAction = UserInterface.displayHomeScreen();
+
+			switch(userAction) {
+				case 1 -> processShowAvailableBooks();
+				case 2 -> processShowCheckedOutBooks();
+				case 3 -> processCheckOutBook();
+				case 4 -> processReturnBook();
+				case 0 -> ifContinue = false;
+			}
+
+		}
+
+	}
+
+	private static void processShowAvailableBooks() {
+		System.out.println("available books");
+	}
+
+	private static void processShowCheckedOutBooks() {
+		System.out.println("Checked out books");
+	}
+
+	private static void processCheckOutBook() {
+		System.out.println("Check out a book");
+	}
+
+	private static void processReturnBook() {
+
+	}
+
 }
