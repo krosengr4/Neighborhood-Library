ae7dec4716cef7420a5b22f3f5ff822685c46fd2
krosengr4 <rosenkev4@gmail.com>
2025-06-30 22:18:35 -0500

    Created methods to display a main and home screen in user interface class

--- NeighborhoodLibrary/src/main/java/UserInterface.java
+++ NeighborhoodLibrary/src/main/java/UserInterface.java
@@ -1,11 +1,22 @@
 public class UserInterface {
 
-	public static String displayHomeScreen() {
-
+	public static String displayMain() {
 		Utils.designLine(70, false);
 		System.out.println("\t\tWELCOME TO THE NEIGHBORHOOD LIBRARY");
 		Utils.designLine(70, true);
-		return Utils.getUserInput("What is your name?: ");
+		return Utils.getUserInput("Enter Your Name: ");
+	}
+
+	public static int displayHomeScreen() {
+		System.out.println("""
+					-----OPTIONS-----
+				1 - Show Available Books
+				2 - Show Checked Out Books
+				3 - Check Out A Book
+				4 - Return A Book
+				0 - Exit
+				""");
+		return Utils.getUserInputInt("Enter your option: ");
 	}
 
 }
