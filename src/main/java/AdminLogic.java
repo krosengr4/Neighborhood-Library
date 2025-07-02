4dd0f903a95da193b3964aaceba8cf8cb0899baf
krosengr4 <rosenkev4@gmail.com>
2025-07-01 20:47:00 -0500

    Added method to handle the admin screen choice with empty skeleton methods for each choice

--- src/main/java/AdminLogic.java
+++ src/main/java/AdminLogic.java
@@ -1,2 +1,30 @@
 public class AdminLogic {
+
+	public static void processAdminOptions() {
+		boolean ifAdminContinue = true;
+
+		while(ifAdminContinue) {
+			int adminChoice = UserInterface.displayAdminScreen();
+
+			switch(adminChoice) {
+				case 1 -> processAddBook();
+				case 2 -> processUpdateBook();
+				case 3 -> processDeleteBook();
+				case 0 -> ifAdminContinue = false;
+			}
+		}
+	}
+
+	private static void processAddBook() {
+		System.out.println("Add Book");
+	}
+
+	private static void processUpdateBook() {
+		System.out.println("Update Book");
+	}
+
+	private static void processDeleteBook() {
+		System.out.println("Delete book");
+	}
+
 }
